6deeaa47cf60c6835c7c627f1fb957f72e38b40d
maruf-ahmed <marufahamed1110@gmail.com>
2024-12-01 21:09:49 +0600

    add academic semester interface

--- src/app/modules/users/user.router.ts
+++ src/app/modules/users/user.router.ts
@@ -1,18 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
 import { usersController } from "./user.controller";
+import { AnyZodObject } from "zod";
 
 const router = express.Router();
 
-const shenaBahini = (name: string) => {
+const validationRequest = (schema: AnyZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    console.log(`i am a shenabahini name ${name}`);
-    next();
+    // Validation code here --------------->
+    const zodParseData = await schema.parseAsync({
+      body: req.body,
+    });
   };
 };
 
 router.post(
   "/create-users",
-  shenaBahini("Validation"),
+    // validationRequest({ }),
   usersController.createStudent
 );
 
