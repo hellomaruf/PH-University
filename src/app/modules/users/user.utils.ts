cd3c8d4e04f737216c4d11459cd95b95b68e26da
maruf-ahmed <marufahamed1110@gmail.com>
2024-12-06 19:11:10 +0600

    update auto generated id

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -17,15 +17,26 @@ const findLastStudentId = async () => {
     .lean();
   return lastStudentId?.id ? lastStudentId.id.substring(6) : undefined;
 };
-console.log(findLastStudentId());
 
 // generate student id ----------->
 export const generateStudentId = async (payload: TAcademicSemester) => {
+  let currentSemesterId = (0).toString();
   // first time id---------->
-  const lastStudentId = await findLastStudentId();
-  const currentId = lastStudentId || (0).toString();
+  const lastStudentId = await findLastStudentId(); // 2024--->year 01---->code 0001----generated id
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4); //01
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //2024
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
 
-  let incrementId = (parseInt(currentId) + 1).toString().padStart(4, "0");
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentSemesterYear === currentSemesterYear
+  ) {
+    currentSemesterId = lastStudentId.substring(6) 
+  }
+
+  let incrementId = (parseInt(currentSemesterId) + 1).toString().padStart(4, "0");
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
 };
