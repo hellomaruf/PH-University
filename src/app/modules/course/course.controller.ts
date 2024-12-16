a6db51b2b4451d662b25a16d7f935828858f9f53
maruf-ahmed <marufahamed1110@gmail.com>
2024-12-16 12:50:50 +0600

    add course controller

---
+++ src/app/modules/course/course.controller.ts
@@ -0,0 +1,56 @@
+import { RequestHandler } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { CourseServices } from "./course.service";
+
+const createCourse: RequestHandler = catchAsync(async (req, res, next) => {
+  const courseData = req.body;
+
+  // will call service function to send this data------------->
+  const result = await CourseServices.createCourseIntoDB(courseData);
+
+  // send response ------------>
+  res.status(200).json({
+    success: true,
+    massage: "Create Course is create successfully",
+    data: result,
+  });
+});
+
+const getAllCourse: RequestHandler = catchAsync(async (req, res, next) => {
+  const result = await CourseServices.getAllCourseFromDB();
+
+  res.status(200).json({
+    success: true,
+    massage: "Get all Course Successfully",
+    data: result,
+  });
+});
+
+const getASingleCourse: RequestHandler = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await CourseServices.getSingleCourseFromDB(id);
+
+  res.status(200).json({
+    success: true,
+    massage: "Get single Course Successfully",
+    data: result,
+  });
+});
+
+const deleteCourse: RequestHandler = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await CourseServices.deleteCourseFromDB(id);
+
+  res.status(200).json({
+    success: true,
+    massage: "Delete Course Successfully",
+    data: result,
+  });
+});
+
+export const academicSemesterController = {
+  createCourse,
+  getAllCourse,
+  getASingleCourse,
+  deleteCourse,
+};
